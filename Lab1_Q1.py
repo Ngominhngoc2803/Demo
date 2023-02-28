17276f550185ba67a24e653505627bcd33e614e2
Ngominhngoc2803 <ngominhngoc2803@gmail.com>
2023-02-28 22:02:10 +0700

    minh ngoc

---
+++ Lab1_Q1.py
@@ -0,0 +1,20 @@
+"""
+VinUniversity
+COMP1010 - Fall 2022 - Introduction to Programming
+Lab[01] - Week[1] - [Getting Started with Python]
+by Ngo Minh Ngoc (V202200669)
+
+Group: [2]
+Date: Oct. 07, 2022
+Disclaimer: I certify that this assignment is my own work and that I ←-
+have not copied in part or whole or otherwise plagiarised the work ←-
+of other students and/or persons.
+"""
+
+"""
+#-----------------------Problem 1--------------------------------
+# Write a program that prints out "Hello , World!"
+#----------------------------------------------------------------
+
+"""
+print("Hello, World!")
